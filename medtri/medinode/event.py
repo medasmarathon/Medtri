78f3acd2469d47fb623ca3636e30607ac7e418c3
Luong Duc <vinhduc91@outlook.com>
2021-08-28 09:39:32 +0700

    update has apriori factor

--- medtri/medinode/event.py
+++ medtri/medinode/event.py
@@ -1,12 +1,13 @@
 from typing import List
+from copy import copy, deepcopy
 from .factor import Factor
 
 class Event:
   def __init__(
     self,
     name : str,
-    apriori_factors : List["Factor"] = None,
-    posteriori_factors : List["Factor"] = None,
+    apriori_factors : List["Factor"] = [],
+    posteriori_factors : List["Factor"] = [],
     prevalence : int = None,
     *args, 
     **kwargs
@@ -16,5 +17,7 @@ class Event:
     self.posteriori_factors = posteriori_factors
     self.prevalence = prevalence
   
-  def has_apriori_factor(self, factor_event : "Event", prevalence : int):
+  def has_apriori_factor(self, factor_event : "Event", factor_prevalence : int) -> "Event":
+    factor = Factor(factor_event, factor_prevalence)
+    self.apriori_factors.append(factor)
     return self
