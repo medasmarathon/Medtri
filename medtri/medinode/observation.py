f0dfdac0736bb73eaaa9c8952a60dd0fab0f7915
Luong Duc <vinhduc91@outlook.com>
2021-08-28 17:17:41 +0700

    add observation

---
+++ medtri/medinode/observation.py
@@ -0,0 +1,14 @@
+from medtri.medinode.constraints.datatype import percentage_value
+from .event import Event
+
+
+class Observation:
+  def __init__(
+      self,
+      event: Event,
+      is_observed: bool = None,
+      observed_probability: float = None
+      ) -> None:
+    self.event = event
+    self.is_observed = is_observed
+    self.observed_probability = percentage_value(observed_probability)
