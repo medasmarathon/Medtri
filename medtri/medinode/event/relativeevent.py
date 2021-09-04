aa3b7cb12fb61b490112f1d21884a4fb926a8ed7
Luong Duc <vinhduc91@outlook.com>
2021-09-04 23:46:16 +0700

    add more clarity

--- medtri/medinode/event/relativeevent.py
+++ medtri/medinode/event/relativeevent.py
@@ -42,19 +42,21 @@ class RelativeEvent(BaseEvent):
       self.apriori_events[existed_apriori_event_index].prevalence = dependent_prevalence
     return self
 
+  def prevalence_relative_to_observations(self, observations: List[IObservation]) -> float:
+    if self.__is_observed_in(observations):
+      return self.prevalence
+    return self.relative_probability_of_observations(observations) * self.prevalence
+
   def relative_probability_of_observations(self, observations: List[IObservation]) -> float:
     obs = observations.copy()
     apriori_list = self.apriori_events.copy()
     prob = 1
-    if self.__is_observed_in(obs):
-      return 1
     for dependent_event in self.apriori_events:
       apriori_list.remove(dependent_event)
       index = dependent_event.index_in_observations(obs)
       if index is not None:
-        prob *= dependent_event.prevalence if obs[index].is_present else (
-            1 - dependent_event.prevalence
-            )
+        prob = prob * dependent_event.prevalence if (obs[index].is_present
+                                                     ) else prob * (1 - dependent_event.prevalence)
         obs.pop(index)
     if any(obs) and any(apriori_list):
       """
@@ -64,12 +66,10 @@ class RelativeEvent(BaseEvent):
         if any(dependent_event.apriori_events):
           """
           If dependent_event has no apriori event -> dependent_event is a pure event --> pass
+          Else (below) calculate the prevalence of dependent event (if could happen) in presence of the remaining observations
           """
           prob *= dependent_event.prevalence_relative_to_observations(obs)
     return prob
-
-  def prevalence_relative_to_observations(self, observations: List[IObservation]) -> float:
-    return self.relative_probability_of_observations(observations) * self.prevalence
 
   def __is_observed_in(self, observations: List[IObservation]) -> bool:
     index = self.index_in_observations(observations)
